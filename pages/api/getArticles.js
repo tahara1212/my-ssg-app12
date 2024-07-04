c3b99bcfa8a72e0ea4e90fda0adb3fc58d86cbf0
sh-tahara <shumpei.tahara@nijibox.co.jp>
2024-07-04 16:07:50 +0900

    microCMSからデータを取得し、ページトップに表示

---
+++ pages/api/getArticles.js
@@ -0,0 +1,12 @@
+
+import { client } from "../..//lib/client";
+
+export const getArticles = async ({
+  limit,
+  fields,
+}) => {
+  return client.get({
+    endpoint: "articles",
+    queries: { fields, limit, orders: "-publishedAt" },
+  });
+};
