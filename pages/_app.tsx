721c100c42351b86bb76345837379ce550f07780
tahara1212 <shunpei.tahara@gmail.com>
2024-07-16 17:16:38 +0900

    GA連携

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
