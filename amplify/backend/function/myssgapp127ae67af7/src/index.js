e0f46443862a0d3ba90e2096dacf51e335f38945
tahara1212 <shunpei.tahara@gmail.com>
2024-07-17 19:55:14 +0900

    Lambda関数のコマンドを変更

--- amplify/backend/function/myssgapp127ae67af7/src/index.js
+++ amplify/backend/function/myssgapp127ae67af7/src/index.js
@@ -1,11 +1,10 @@
 const { exec } = require('child_process');
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../../../../../.env.local') });
 
 exports.handler = async (event) => {
     return new Promise((resolve, reject) => {
-        const scriptPath = path.join(__dirname, '../../../../../scripts/add_articles.ts');
-        const command = `./node_modules/.bin/tsimp --import=tsimp/import ${scriptPath}`;
+        const scriptPath = path.join(__dirname, '../scripts/add_articles.ts');
+        const command = `node --env-file=${path.join(__dirname, '../.env.local')} --import=tsimp/import ${scriptPath}`;
 
         exec(command, { cwd: __dirname }, (error, stdout, stderr) => {
             if (error) {
